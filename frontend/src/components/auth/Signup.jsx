fc092b2dd4dd12c0c4a13f0885ed085a2438d3f2
Md Ashraf <ashhiimd@gmail.com>
2024-09-12 21:26:19 +0530

    final update

--- frontend/src/components/auth/Signup.jsx
+++ frontend/src/components/auth/Signup.jsx
@@ -80,7 +80,7 @@ const Signup = () => {
                             value={input.fullname}
                             name="fullname"
                             onChange={changeEventHandler}
-                            placeholder="ashrafl"
+                            placeholder="ashraf"
                         />
                     </div>
                     <div className='my-2'>
@@ -90,7 +90,7 @@ const Signup = () => {
                             value={input.email}
                             name="email"
                             onChange={changeEventHandler}
-                            placeholder="ashrafl@gmail.com"
+                            placeholder="ashraf@gmail.com"
                         />
                     </div>
                     <div className='my-2'>
@@ -110,7 +110,7 @@ const Signup = () => {
                             value={input.password}
                             name="password"
                             onChange={changeEventHandler}
-                            placeholder="ashrafl@gmail.com"
+                            placeholder="ashraf@gmail.com"
                         />
                     </div>
                     <div className='flex items-center justify-between'>
